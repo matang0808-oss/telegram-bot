7958afac4f901c478f78e5c66be8bfc033ebef72
matang0808-oss <matang0808@gmail.com>
2026-05-11 18:59:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import requests
-import time
 
 TOKEN = "8600328140:AAEE18bEM6CslSAhha5U5_UeM9rDkCfCvbE"
 CHAT_ID = "1003904944480"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+url = f"https://api.telegram.org/bot{8600328140:AAEE18bEM6CslSAhha5U5_UeM9rDkCfCvbE}/sendMessage"
 
-while True:
+data = {
+    "chat_id": 1003904944480,
+    "text": "🔥 BLACK168 พร้อมให้บริการ 24 ชั่วโมง 🔥"
+}
 
-    data = {
-        "chat_id": 1003904944480,
-        "text": "🔥 BLACK168 พร้อมให้บริการ 24 ชั่วโมง 🔥"
-    }
+requests.post(url, data=data)
 
-    requests.post(url, data=data)
-
-    print("ส่งข้อความแล้ว")
-
-    time.sleep(60)
+print("ส่งข้อความสำเร็จ")
